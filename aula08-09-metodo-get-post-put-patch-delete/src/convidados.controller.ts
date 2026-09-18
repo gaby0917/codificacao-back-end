e7593faa712a63634ce46d14f51075f34602d327
Gabrielly Amaral <gabyamaral2805@gmail.com>
2026-09-18 17:28:23 -0300

    feat: Adiciona ferramentas novas ao projeto

---
+++ aula08-09-metodo-get-post-put-patch-delete/src/convidados.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Patch, Delete, Param, Body, HttpCode } from "@nestjs/common";
+import { ConvidadosService } from "./convidados.service.js";
+import { CriarConvidadoDto } from "./dto/criar-convidado.dto.js";
+
+@Controller('convidados')
+export class ConvidadosController {
+    constructor(private readonly convidadosService: ConvidadosService){}
+        @Get()
+        listarConvidados(){
+            return this.convidadosService.finAll();
+        }
+
+        @Post()
+        criar(@Body() criarConvidadoDto: CriarConvidadoDto){
+            console.log(`[OPERADOR]: Novo convidado recebido: ${criarConvidadoDto.nome}`);
+
+            return{
+                mensagem: `Convidado ${criarConvidadoDto.nome} adicionado com sucesso`,
+                dados: criarConvidadoDto,
+            }
+        }
+
+        @Patch()
+        atualizarIdade(@Param('id') id: string, @Body('idade') idade: number){
+            console.log('[ADMINISTRADOR]: Atualizando a idade do ID ${id}');
+            return this.convidadosService.atualizarIdade(+id, idade);
+        }
+
+        @Delete(':id')
+        @HttpCode(204)
+        remover(@Param('id') id: number){
+            console.log(`[ADMINISTRADOR]: Removendo Convidado ID: ${id}`);
+            this.convidadosService.removerConvidado(+id);
+        }
+}
